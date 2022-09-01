4d2c86528f619313d5d77436dfd2927916fe806d
Hanan Fokkens <hananfokkens@gmail.com>
2022-09-01 12:53:23 +1200

    added tags to the api posts and posts/slug
    added helper function read_markdown_tags in markdown_helper.
    This is used to work out the most common words in file, and removes the markdown and stop words.

--- blog/posts/markdown_helper.py
+++ blog/posts/markdown_helper.py
@@ -1,5 +1,29 @@
 """Helper functions for working with markdown."""
 from io import TextIOWrapper
+from collections import Counter
+import re
+
+STOP_WORDS = [
+    "#", "##", "a", "about", "above", "after", "again", "against", "all", "am",
+    "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been",
+    "before", "being", "below", "between", "both", "but", "by", "can't", "cannot",
+    "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't",
+    "down", "during", "each", "few", "for", "from", "further", "had", "hadn't",
+    "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's",
+    "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how",
+    "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't",
+    "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my",
+    "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other",
+    "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she",
+    "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that",
+    "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's",
+    "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through",
+    "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll",
+    "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where",
+    "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't",
+    "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours",
+    "yourself", "yourselves"
+]
 
 def read_markdown_meta_data(file: TextIOWrapper):
     """This takes a markdown file and reads the meta-data off the top of the file.
@@ -26,3 +50,28 @@ def read_markdown_meta_data(file: TextIOWrapper):
         # seek back to the start of the file.
         file.seek(0)
     return meta
+
+def read_markdown_tags(file: TextIOWrapper, revert_position=False):
+    """This takes a markdown file and returns the keywords from that file. 
+    If revert_position is True the file position will be reverted to the 
+    position it was when passed into this function. """
+    if revert_position:
+        # save the file position for later.
+        initial_position = file.tell()
+    
+    # read and lower all words because 'This' should be the same as 'this'
+    words = file.read().lower()
+    # remove all non alpha characters excluding space, \n, and "'" (because you'd won't ...)
+    words = re.sub(r"[^a-z'\s]", "", words)
+    words_split = words.split()
+    # use Counter to count the number of duplicates in the list
+    word_count = Counter(words_split)
+    for stop_word in STOP_WORDS:
+        del word_count[stop_word]
+
+    tags = [word for word, count in word_count.most_common(5)]
+    
+    if revert_position:
+        file.seek(initial_position)
+    return tags
+
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -4,30 +4,7 @@ import markdown
 from blog.settings import BASE_DIR
 from django.http import JsonResponse
 
-from posts.markdown_helper import read_markdown_meta_data
-
-# Feel free to move this to a new file if you are carrying out the 'tags' calculation there
-STOP_WORDS = [
-    "#", "##", "a", "about", "above", "after", "again", "against", "all", "am",
-    "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been",
-    "before", "being", "below", "between", "both", "but", "by", "can't", "cannot",
-    "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't",
-    "down", "during", "each", "few", "for", "from", "further", "had", "hadn't",
-    "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's",
-    "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how",
-    "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't",
-    "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my",
-    "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other",
-    "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she",
-    "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that",
-    "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's",
-    "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through",
-    "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll",
-    "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where",
-    "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't",
-    "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours",
-    "yourself", "yourselves"
-]
+from posts.markdown_helper import read_markdown_meta_data, read_markdown_tags
 
 
 def post(request, slug):
@@ -36,6 +13,9 @@ def post(request, slug):
         .joinpath(slug + '.md')
     with file_path.open("r") as file:
         meta_data = read_markdown_meta_data(file)
+        # revert the position for the markdown to html function.
+        tags = read_markdown_tags(file, revert_position=True)
+        meta_data['tags'] = tags
         html = markdown.markdown(file.read())
     meta_data["html"] = html
     return JsonResponse(meta_data)
@@ -49,6 +29,8 @@ def posts(request):
         if file_path.is_file():
             with file_path.open("r") as file:
                 meta_data = read_markdown_meta_data(file)
+                tags = read_markdown_tags(file)
+                meta_data['tags'] = tags
                 posts.append(meta_data)
 
     return JsonResponse({
